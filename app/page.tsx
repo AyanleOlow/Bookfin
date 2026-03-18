bada9a8a68fbffff29b8d594623899d928070150
Ayanle Olow <randomrandomo156@gmail.com>
2026-03-18 10:01:15 +0100

    man kan se forskjellene mellom pdf og epub og man bare ser 2 typer av filer

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,37 @@
-import Image from "next/image";
+"use client"
 
-const res = await fetch("http://localhost:3000/books")
-const books = await res.json()
+import { useState } from "react"
+import '../app/page.css'
 
 export default function Home() {
+
+  const [files, setFiles] = useState<File[]>([])
+
+  const handleFolder = (event: any) => {
+    const selectedFiles = Array.from(event.target.files) as File[]
+    setFiles(selectedFiles)
+  }
+
   return (
-    <div></div>
-  );
+    <div style={{ padding: "20px" }}>
+
+      <h1>Select Book Folder</h1>
+
+      <input
+        type="file"
+        multiple
+        onChange={handleFolder}
+        {...{ webkitdirectory: "true" }}
+      />
+
+      <h2>Books Found</h2>
+
+      <ul className="">
+        {files.map((file, index) => (
+          <li className="flex justify-center border-width: 5px whi ; " key={index}>{file.name}</li>
+        ))}
+      </ul>
+
+    </div>
+  )
 }
